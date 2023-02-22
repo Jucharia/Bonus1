39806c15c729c733f655d77878eeb9fef5a61d68
Yuliya Mylnikova <jucharia87@gmail.com>
2023-02-22 21:39:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int mile = 20; //за каждые 20 рублей начисляется 1 миля
+        int ticket = 17629; //стоимость билета
+
+        System.out.println ((ticket/mile) + " начисленные бонусные мили" );
+
+        int answer = 881;
+    }
+}
