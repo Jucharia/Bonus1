d779584ba8d04cc31d6c07026ea4c2ec277190d4
Yuliya Mylnikova <jucharia87@gmail.com>
2023-02-25 15:34:49 +0300

    Add BonusMilesService

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int bonus = 20;
+        int result;
+        result = price / bonus;
+        return result;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int mile = 20; //за каждые 20 рублей начисляется 1 миля
-        int ticket = 17629; //стоимость билета
-
-        System.out.println((ticket / mile) + " начисленные бонусные мили");
-
-        int answer = 881;
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
