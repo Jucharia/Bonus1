f9ee8b17560466f2a6ce668b2759b60f78deabad
Yuliya Mylnikova <jucharia87@gmail.com>
2023-02-26 16:07:47 +0300

    Changed BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,6 @@
 public class BonusMilesService {
-    public int calculate(int price, int bonus) {
+    public int calculate(int price) {
+        int bonus = 20;
         int result;
         result = price / bonus;
         return result;
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int bonus = 20;
-        int miles = service.calculate(price, bonus); // 500 начисленных миль
+        int miles = service.calculate(price); // 500 начисленных миль
         System.out.println(miles);
     }
 }
